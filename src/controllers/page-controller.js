6f9db8b3e10e6dbf5415c135535723d6d02e8aee
Daria Glagol <daria.glagol@gmail.com>
2020-01-02 00:34:46 +0700

    add controller

---
+++ src/controllers/page-controller.js
@@ -0,0 +1,91 @@
+import FilmCard from "../components/film-card";
+import FilmPopup from "../components/film-popup-template";
+import TopFilm from "../components/top-film";
+import ShowMoreButton from "../components/show-more-button";
+import NoData from "../components/no-data";
+import {remove, render} from "../utilities/render";
+import {
+  CARDS_COUNT,
+  CLICKABLE_ITEMS,
+  RATES_CARDS_COUNT,
+  RenderPosition,
+  TopFilmTypes,
+  TOTAL_FILM_COUNT,
+} from "../mocks/constants";
+import {setCardClickEventListeners} from "../utilities/utilities";
+
+const renderFilm = (film, filmRenderPlace, popupRenderPlace) => {
+  const card = new FilmCard(film);
+  const filmPopup = new FilmPopup(film, popupRenderPlace);
+
+  const onClosePopup = () => {
+    filmPopup.getElement().remove();
+    filmPopup.removeElement();
+
+    document.removeEventListener(`keydown`, onClosePopup);
+  };
+
+  const onOpenPopup = () => {
+    render(popupRenderPlace, filmPopup.getElement(), RenderPosition.BEFORE_END);
+    const closePopupButton = filmPopup.getElement().querySelector(`.film-details__close-btn`);
+    closePopupButton.addEventListener(`click`, onClosePopup);
+
+    document.addEventListener(`keydown`, onClosePopup);
+  };
+
+  setCardClickEventListeners(CLICKABLE_ITEMS, card, onOpenPopup);
+
+  render(filmRenderPlace, card.getElement(), RenderPosition.BEFORE_END);
+};
+
+const renderFilms = (films, filmRenderPlace, popupRenderPlace, sliceCount, slicePoint = 0) => {
+  films.slice(slicePoint, slicePoint + sliceCount).forEach((film) => {
+    renderFilm(film, filmRenderPlace, popupRenderPlace);
+  });
+};
+
+export default class PageController {
+  constructor(container) {
+    this._container = container;
+    this._showMoreButton = new ShowMoreButton();
+  }
+
+  render(generatedFilms) {
+    const topRatedRenderPlace = this._container.querySelector(`.films`);
+    const buttonRenderPlace = this._container.querySelector(`.films-list`);
+    const filmsRenderPlace = this._container.querySelector(`.films-list__container`);
+
+    if (generatedFilms.length < 1) {
+      render(buttonRenderPlace, new NoData().getElement(), RenderPosition.BEFORE_END);
+    } else {
+      let startPointSlice = 0;
+      renderFilms(generatedFilms, filmsRenderPlace, this._container, CARDS_COUNT, startPointSlice);
+
+      const ratedFilms = new TopFilm(generatedFilms, TopFilmTypes.RATING);
+      const mostCommentedFilms = new TopFilm(generatedFilms, TopFilmTypes.COMMENTS);
+
+      render(topRatedRenderPlace, ratedFilms.getElement(), RenderPosition.BEFORE_END);
+      render(topRatedRenderPlace, mostCommentedFilms.getElement(), RenderPosition.BEFORE_END);
+
+      const ratingPlace = ratedFilms.getElement().querySelector(`.films-list__container`);
+      const commentsPlace = mostCommentedFilms.getElement().querySelector(`.films-list__container`);
+
+      renderFilms(ratedFilms.getTopFilms(), ratingPlace, this._container, RATES_CARDS_COUNT);
+      renderFilms(mostCommentedFilms.getTopFilms(), commentsPlace, this._container, RATES_CARDS_COUNT);
+
+      render(buttonRenderPlace, this._showMoreButton.getElement(), RenderPosition.BEFORE_END);
+
+      this._showMoreButton.setShowMoreButtonClickHandler(() => {
+        startPointSlice = startPointSlice <= TOTAL_FILM_COUNT - CARDS_COUNT
+          ? startPointSlice + CARDS_COUNT
+          : TOTAL_FILM_COUNT;
+
+        if (startPointSlice + CARDS_COUNT > TOTAL_FILM_COUNT) {
+          remove(this._showMoreButton);
+        }
+
+        renderFilms(generatedFilms, filmsRenderPlace, this._container, CARDS_COUNT, startPointSlice);
+      });
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 import UserProfile from './components/user-profile';
 import Menu from './components/menu';
 import FilmsList from './components/films-lists';
-import ShowMoreButton from './components/show-more-button';
-import TopFilm from './components/top-film';
-import FilmCard from "./components/film-card";
-import FilmPopup from "./components/film-popup-template";
-import NoData from "./components/no-data";
+import PageController from "./controllers/page-controller";
 import Sorting from "./components/sorting";
-import {CARDS_COUNT, RATES_CARDS_COUNT, CLICKABLE_ITEMS, RenderPosition, TOTAL_FILM_COUNT, TopFilmTypes} from './mocks/constants';
+import {RenderPosition, TOTAL_FILM_COUNT} from './mocks/constants';
 import {generateFilms} from "./mocks/film";
 import {generateFilters} from './mocks/filters';
-import {setCardClickEventListeners} from './utilities/utilities';
-import {render, remove} from './utilities/render';
-
-let startPointSlice = 0;
+import {render} from './utilities/render';
 
 const headerNode = document.querySelector(`.header`);
 const mainNode = document.querySelector(`.main`);
@@ -26,78 +19,14 @@ const filters = generateFilters(generatedFilms);
 const watchedFilms = filters.history;
 
 const filmList = new FilmsList();
-const showMoreButton = new ShowMoreButton();
 
 const footerStatistic = footerNode.querySelector(`.footer__statistics p`);
 footerStatistic.innerText = `${generatedFilms.length} movies inside`;
-
-const renderFilm = (film, filmRenderPlace, popupRenderPlace) => {
-  const card = new FilmCard(film);
-  const filmPopup = new FilmPopup(film, popupRenderPlace);
-
-  const onClosePopup = () => {
-    filmPopup.getElement().remove();
-    filmPopup.removeElement();
-
-    document.removeEventListener(`keydown`, onClosePopup);
-  };
-
-  const onOpenPopup = () => {
-    render(mainNode, filmPopup.getElement(), RenderPosition.BEFORE_END);
-    const closePopupButton = filmPopup.getElement().querySelector(`.film-details__close-btn`);
-    closePopupButton.addEventListener(`click`, onClosePopup);
-
-    document.addEventListener(`keydown`, onClosePopup);
-  };
-
-  setCardClickEventListeners(CLICKABLE_ITEMS, card, onOpenPopup);
-
-  render(filmRenderPlace, card.getElement(), RenderPosition.BEFORE_END);
-};
-
-const renderFilms = (films, filmRenderPlace, popupRenderPlace, sliceCount, slicePoint = 0) => {
-  films.slice(slicePoint, slicePoint + sliceCount).forEach((film) => {
-    renderFilm(film, filmRenderPlace, popupRenderPlace);
-  });
-};
 
 render(headerNode, new UserProfile(watchedFilms).getElement(), RenderPosition.BEFORE_END);
 render(mainNode, new Menu(generatedFilms, filters).getElement(), RenderPosition.AFTER_BEGIN);
 render(mainNode, new Sorting().getElement(), RenderPosition.BEFORE_END);
 render(mainNode, filmList.getElement(), RenderPosition.BEFORE_END);
-const buttonRenderPlace = filmList.getElement().querySelector(`.films-list`);
-const filmsRenderPlace = filmList.getElement().querySelector(`.films-list__container`);
 
-if (generatedFilms.length < 1) {
-  render(buttonRenderPlace, new NoData().getElement(), RenderPosition.BEFORE_END);
-} else {
-  renderFilms(generatedFilms, filmsRenderPlace, mainNode, CARDS_COUNT, startPointSlice);
-
-  const ratedFilms = new TopFilm(generatedFilms, TopFilmTypes.RATING, filmList.getElement());
-  const mostCommentedFilms = new TopFilm(generatedFilms, TopFilmTypes.COMMENTS, filmList.getElement());
-
-  render(filmList.getElement(), ratedFilms.getElement(), RenderPosition.BEFORE_END);
-  render(filmList.getElement(), mostCommentedFilms.getElement(), RenderPosition.BEFORE_END);
-
-  const ratingPlace = ratedFilms.getElement().querySelector(`.films-list__container`);
-  const commentsPlace = mostCommentedFilms.getElement().querySelector(`.films-list__container`);
-
-  renderFilms(ratedFilms.getTopFilms(), ratingPlace, mainNode, RATES_CARDS_COUNT);
-  renderFilms(mostCommentedFilms.getTopFilms(), commentsPlace, mainNode, RATES_CARDS_COUNT);
-
-  render(buttonRenderPlace, showMoreButton.getElement(), RenderPosition.BEFORE_END);
-
-  showMoreButton.setShowMoreButtonClickHandler(() => {
-    startPointSlice = startPointSlice <= TOTAL_FILM_COUNT - CARDS_COUNT
-      ? startPointSlice + CARDS_COUNT
-      : TOTAL_FILM_COUNT;
-
-    if (startPointSlice + CARDS_COUNT > TOTAL_FILM_COUNT) {
-      remove(showMoreButton);
-    }
-
-    renderFilms(generatedFilms, filmsRenderPlace, mainNode, CARDS_COUNT, startPointSlice);
-  });
-}
-
-
+const pageController = new PageController(mainNode);
+pageController.render(generatedFilms);
