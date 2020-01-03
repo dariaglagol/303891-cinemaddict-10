58f9b1d5fd8f0d584632379278ed04e09e7063a0
Daria Glagol <daria.glagol@gmail.com>
2020-01-03 20:09:31 +0700

    add logic to sorting

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,46 @@
 import AbstractComponent from "./abstract-component";
+import {SortType} from '../mocks/constants';
 
 const createSortingTemplate = () => {
   return (
     `<ul class="sort">
-        <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-        <li><a href="#" class="sort__button">Sort by date</a></li>
-        <li><a href="#" class="sort__button">Sort by rating</a></li>
+        <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+        <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+        <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
     </ul>`
   );
 };
 
 export default class Sorting extends AbstractComponent {
-
   getTemplate() {
     return createSortingTemplate();
+  }
+
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      const target = evt.target;
+      const sortType = evt.target.dataset.sortType;
+      const siblingsElements = Array.from(target.parentNode.parentNode.children);
+
+      if (target.tagName !== `A`) {
+        return;
+      }
+
+      if (this._currenSortType === sortType) {
+        return;
+      }
+
+      siblingsElements.forEach((el) => {
+        el.firstElementChild.classList.remove(`sort__button--active`);
+      });
+
+      this._currenSortType = sortType;
+
+      target.classList.add(`sort__button--active`);
+
+      handler(this._currenSortType);
+    });
   }
 }
